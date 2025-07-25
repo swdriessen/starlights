d9dd8aee52451c577445529591140485a3ec69fc
Bas Driessen <bas_driessen@hotmail.com>
2025-07-25 19:03:30 +0200

    add platform test project

---
+++ src/platform/Starlights.Platform.Tests/PlatformTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Starlights.Platform.Hosting;
+
+namespace Starlights.Platform.Tests;
+
+[TestClass]
+public sealed class PlatformTests
+{
+    [TestMethod]
+    public void AddStarlightsPlatform()
+    {
+        // Arrange
+        var builder = Host.CreateApplicationBuilder();
+
+        // Act
+        builder.AddStarlightsPlatform();
+
+        // Assert
+        var app = builder.Build();
+        Assert.IsNotNull(app.Services);
+    }
+}
