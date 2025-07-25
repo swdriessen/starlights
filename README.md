b5d62ff93cf07e84816406744fde9fdb4893fa80
Bas Driessen <bas_driessen@hotmail.com>
2025-07-25 15:20:41 +0200

    add solution and application entrypoint

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# starlights
+# Project Starlights
+
+A project to create an online toolset to enhance table top role playing games with an initial focus on Dungeons & Dragons.
+
+This project will lean heavily on previous experience with [Aurora](https://www.aurorabuilder.com) and recent prototyping work with web technologies.
---
+++ src/apps/Starlights.Application/Program.cs
@@ -0,0 +1,37 @@
+
+namespace Starlights.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapGet("/hello", (HttpContext httpContext) =>
+            {
+                return Results.Ok("Hello, World!");
+            })
+            .WithName("HelloWorld");
+
+            app.Run();
+        }
+    }
+}
