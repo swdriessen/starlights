63054e47d2cf5142796aedbe9268f8e07126093b
Bas Driessen <bas_driessen@hotmail.com>
2025-07-26 11:13:30 +0200

    add elements module projects

---
+++ src/modules/elements/Modules.Elements.Tests/ElementsTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace Modules.Elements.Tests;
+
+[TestClass]
+public sealed class ElementsTests
+{
+    [TestMethod]
+    public void Create()
+    {
+        // Arrange
+        var elementName = "TestElement";
+
+        // Act
+        var element = new Element(elementName);
+
+        // Assert
+        element.Name.Should().Be(elementName);
+    }
+}
---
+++ src/modules/elements/Modules.Elements/Element.cs
@@ -0,0 +1,16 @@
+﻿using Starlights.Platform.Domain;
+
+namespace Modules.Elements;
+
+public class Element : AggregateRoot<Guid>
+{
+    public Element(string name)
+    {
+        Name = name;
+    }
+
+    /// <summary>
+    /// Gets the name of the element.
+    /// </summary>
+    public string Name { get; }
+}
