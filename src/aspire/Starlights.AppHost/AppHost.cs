75512fadaeb4bc24698bc666d134b9cd7b1768ab
Bas Driessen <bas_driessen@hotmail.com>
2025-08-01 17:33:54 +0200

    wait for the migrations to complete before starting the application

--- src/aspire/Starlights.AppHost/AppHost.cs
+++ src/aspire/Starlights.AppHost/AppHost.cs
@@ -4,16 +4,24 @@ var database = builder.AddSqlServer("sqlserver")
     .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase("starlights-db", "starlights-db");
 
+IResourceBuilder<ProjectResource> migrationService = null!;
 
 if (builder.ExecutionContext.IsRunMode)
 {
-    builder.AddProject<Projects.Modules_Elements_Data_EntityFramework_MigrationService>("elements-migrationservice")
+    // run migrations in development environment to ensure the database is up-to-date
+    migrationService = builder.AddProject<Projects.Modules_Elements_Data_EntityFramework_MigrationService>("elements-migrations")
         .WithReference(database)
         .WaitFor(database);
 }
 
-builder.AddProject<Projects.Starlights_Application>("starlights-application")
+var application = builder.AddProject<Projects.Starlights_Application>("starlights-application")
     .WithReference(database)
     .WaitFor(database);
+
+if (builder.ExecutionContext.IsRunMode && migrationService is not null)
+{
+    // wait for the migration service to complete before starting the application
+    application.WaitForCompletion(migrationService);
+}
 
 builder.Build().Run();
