3daa8e36669bc47ff2d6725109f9b885d57a82c5
Bas Driessen <bas_driessen@hotmail.com>
2025-08-03 01:24:46 +0200

    add option to log activities in integration tests

--- src/integration/Starlights.Integration.Tests/IntegrationHostBuilder.cs
+++ src/integration/Starlights.Integration.Tests/IntegrationHostBuilder.cs
@@ -5,11 +5,28 @@
 /// </summary>
 public class IntegrationHostBuilder
 {
+    private readonly List<Action<IntegrationHostOptions>> _configureActions = [];
+
+    /// <summary>
+    /// Configures the integration host to use a console activity processor for OpenTelemetry tracing.
+    /// </summary>
+    public IntegrationHostBuilder WithConsoleActivityProcessor()
+    {
+        _configureActions.Add(o => o.UseConsoleActivityProcessor = true);
+        return this;
+    }
+
     /// <summary>
     /// Builds the integration host.
     /// </summary>
     public IntegrationHost Build()
     {
-        return new IntegrationHost();
+        return new IntegrationHost(options =>
+        {
+            foreach (var configureAction in _configureActions)
+            {
+                configureAction(options);
+            }
+        });
     }
 }
