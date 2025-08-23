cb9eab1796af1ae47556f2cb8338f93015226fed
Bas Driessen <bas_driessen@hotmail.com>
2025-08-23 14:57:37 +0200

    add ProgressionComponent

---
+++ src/modules/characters/Modules.Characters.Domain/Progression/ProgressionComponent.cs
@@ -0,0 +1,31 @@
+﻿using Starlights.Modules.Characters.Domain.Characters;
+using Starlights.Modules.Characters.Domain.Components;
+
+namespace Starlights.Modules.Characters.Domain.Progression;
+
+public class ProgressionComponent : CharacterComponentBase
+{
+    public ProgressionComponent(CharacterId parentCharacter)
+        : base(parentCharacter)
+    {
+
+    }
+
+    /// <summary>
+    /// Gets the current level of the character.
+    /// </summary>
+    public int CharacterLevel { get; private set; }
+
+    /// <summary>
+    /// Sets the character's level to the specified value.
+    /// </summary>
+    public void SetCharacterLevel(int level)
+    {
+        if (level < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), "Character level cannot be negative.");
+        }
+
+        CharacterLevel = level;
+    }
+}
