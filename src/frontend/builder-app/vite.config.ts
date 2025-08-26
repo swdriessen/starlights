740e686563b25c2f19c17a38f6af2488f11ee38c
Bas Driessen <bas_driessen@hotmail.com>
2025-08-27 01:31:52 +0200

    add react app to aspire orchestration

--- src/frontend/builder-app/vite.config.ts
+++ src/frontend/builder-app/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { env } from "process";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(env.PORT || "55052"),
+    open: true,
+  },
+});
