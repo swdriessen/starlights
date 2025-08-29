81f2624405312f5ffa587fa8c0b8e1061b20500d
Bas Driessen <bas_driessen@hotmail.com>
2025-08-29 23:46:18 +0200

    add dark/light theme toggle

--- src/frontend/builder-app/src/App.tsx
+++ src/frontend/builder-app/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { usePlatformStatus } from "./lib/queries";
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,29 +12,32 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Project Starlights</h1>
-        <div className="flex items-center gap-2">
-          <Button size={"sm"} onClick={() => setCount((c) => c + 1)}>
-            {count}
-          </Button>
-          <Button size={"sm"} onClick={() => refetch()}>
-            Refresh Status
-          </Button>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div>
+          <h1>Project Starlights</h1>
+          <div className="flex items-center gap-2">
+            <Button size="default" onClick={() => setCount((c) => c + 1)}>
+              {count}
+            </Button>
+            <Button size="default" onClick={() => refetch()}>
+              Refresh Status
+            </Button>
+            <ModeToggle />
+          </div>
         </div>
-      </div>
-      <hr className="my-4" />
-      <div>
-        <h2>Platform</h2>
-        {isLoading && <p>Loading…</p>}
-        {isError && <p>Error: {error?.message}</p>}
-        {status && (
-          <>
-            <p>{status.message}</p>
-            <p>{status.timestamp}</p>
-          </>
-        )}
-      </div>
+        <hr className="my-4" />
+        <div>
+          <h2>Platform</h2>
+          {isLoading && <p>Loading…</p>}
+          {isError && <p>Error: {error?.message}</p>}
+          {status && (
+            <>
+              <p>{status.message}</p>
+              <p>{status.timestamp}</p>
+            </>
+          )}
+        </div>
+      </ThemeProvider>
     </>
   );
 }
