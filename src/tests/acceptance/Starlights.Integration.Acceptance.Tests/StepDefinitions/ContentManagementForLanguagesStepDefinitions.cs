97755c2c9ba20b6bd3ff819de17c6468c0f1b627
Bas Driessen <bas_driessen@hotmail.com>
2025-12-22 01:25:43 +0200

    feat: add a acceptance file for languages

---
+++ src/tests/acceptance/Starlights.Integration.Acceptance.Tests/StepDefinitions/ContentManagementForLanguagesStepDefinitions.cs
@@ -0,0 +1,35 @@
+namespace Starlights.Integration.Acceptance.Tests.StepDefinitions;
+
+[Binding]
+public class ContentManagementForLanguagesStepDefinitions
+{
+    [When("the content creator creates a language with the following properties")]
+    public void WhenTheContentCreatorCreatesALanguageWithTheFollowingProperties(DataTable dataTable)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then("the language appears in the language list with all provided properties")]
+    public void ThenTheLanguageAppearsInTheLanguageListWithAllProvidedProperties()
+    {
+        throw new PendingStepException();
+    }
+
+    [Given("a language exists that includes the following properties")]
+    public void GivenALanguageExistsThatIncludesTheFollowingProperties(DataTable dataTable)
+    {
+        throw new PendingStepException();
+    }
+
+    [When("the content creator updates the language to have the following properties")]
+    public void WhenTheContentCreatorUpdatesTheLanguageToHaveTheFollowingProperties(DataTable dataTable)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then("the language in the language list should have the following properties")]
+    public void ThenTheLanguageInTheLanguageListShouldHaveTheFollowingProperties(DataTable dataTable)
+    {
+        throw new PendingStepException();
+    }
+}
