359d4f394932a67389ad94548d7040da077a371d
Bas Driessen <bas_driessen@hotmail.com>
2025-12-29 02:19:05 +0200

    feat: remove old ability creation and retrieval endpoints

--- src/modules/elements/Modules.Elements.Endpoints/Entities/Abilities/Create/CreateAbilityRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Starlights.Modules.Elements.Endpoints.Entities.Abilities.Create;
-
-public record CreateAbilityRequest
-{
-    public string Name { get; init; } = string.Empty;
-    public string Abbreviation { get; init; } = string.Empty;
-}
--- src/modules/elements/Modules.Elements.Endpoints/Entities/Abilities/GetAbilities/GetAbilitiesResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using Starlights.Modules.Elements.Integration.Models;
-
-namespace Starlights.Modules.Elements.Endpoints.Entities.Abilities.GetAbilities;
-
-public record GetAbilitiesResponse
-{
-    public List<AbilityDataModel> Abilities { get; init; } = [];
-}
