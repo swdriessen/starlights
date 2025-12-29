ce94ac1094a08f6eb0dbe6049fc03974872d88e6
Bas Driessen <bas_driessen@hotmail.com>
2025-12-29 02:17:37 +0200

    feat: add ProficiencyAttributesComponent migration

--- src/modules/elements/Modules.Elements.Data.EntityFramework/Migrations/20251229001345_AddProficiencyAttributes.Designer.cs
+++ src/modules/elements/Modules.Elements.Data.EntityFramework/Migrations/20251229001345_AddProficiencyAttributes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Starlights.Modules.Elements.Data.EntityFramework;
 
@@ -11,9 +12,11 @@ using Starlights.Modules.Elements.Data.EntityFramework;
 namespace Starlights.Modules.Elements.Data.EntityFramework.Migrations
 {
     [DbContext(typeof(ElementsContext))]
-    partial class ElementsContextModelSnapshot : ModelSnapshot
+    [Migration("20251229001345_AddProficiencyAttributes")]
+    partial class AddProficiencyAttributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
