7aeb27b211a3b513812718f73a381a8a62157af1
Bas Driessen <bas_driessen@hotmail.com>
2026-04-12 00:38:44 +0300

    feat: migrate content-manager app to shared ui-framework

--- src/frontend/apps/content-manager/src/main.tsx
+++ src/frontend/apps/content-manager/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-import "./index.css"
+import { ThemeProvider } from "ui-framework"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/frontend/packages/ui-framework/src/index.ts
+++ src/frontend/packages/ui-framework/src/index.ts
@@ -1,4 +1,6 @@
 import "./index.css";
 
+export { ThemeProvider, useTheme } from "./components/theme-provider";
 export * from "./components/ui/button";
 export { cn } from "./lib/utils";
+
