07faeb1263ffdb8d98f46a9b856c4797cd75dc86
Bas Driessen <bas_driessen@hotmail.com>
2026-04-23 22:00:02 +0200

    feat: add global filter for cancelled requests (499)

---
+++ src/platform/components/Starlights.Platform.Components.FastEndpoints/OperationCancelledFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Starlights.Platform.Components.FastEndpoints;
+
+internal sealed class OperationCancelledFilter : IEndpointFilter
+{
+    private readonly ILogger<OperationCancelledFilter> _logger;
+
+    public OperationCancelledFilter(ILogger<OperationCancelledFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        try
+        {
+            return await next(context);
+        }
+        catch (OperationCanceledException ex)
+        {
+            _logger.LogDebug(ex, "The endpoint request was cancelled!");
+            return Results.StatusCode(499);
+        }
+    }
+}
